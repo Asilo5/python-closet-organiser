2dcb759d9944758693789e647435fcb0ca6d5aaf
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-04-15 23:58:55 -0600

    Fix import and start method to add title to window and change size

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import tkinter as tk 
-import PIL import Image, ImageTk 
+from PIL import Image, ImageTk 
 from playsound import playsound
 
 class WardrobeApp:
+    def __init__(self, root):
+       self.root = root
+
+    def create_background(self):
+        # add title to window and change size
+
+        
+
+
 root = tk.Tk()
+app = WardrobeApp(root)
+root.mainloop()
