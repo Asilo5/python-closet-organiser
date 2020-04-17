0eea476177ba37a29a87cf5f55bd1b1258e0cf5b
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-04-16 21:04:21 -0600

    Add width and height to screen and try to fix import issues with all imports

--- app.py
+++ app.py
@@ -2,14 +2,21 @@ import tkinter as tk
 from PIL import Image, ImageTk 
 from playsound import playsound
 
+WINDOW_TITLE = "My Wardrobe"
+WINDOW_HEIGHT = 220
+WINDOW_WIDTH = 500
+
 class WardrobeApp:
     def __init__(self, root):
        self.root = root
 
+       # create background
+       self.create_background()
+
     def create_background(self):
         # add title to window and change size
-
-        
+        self.root.title(WINDOW_TITLE)
+        self.root.geometry('{0}x{1}'.format(WINDOW_WIDTH, WINDOW_HEIGHT))
 
 
 root = tk.Tk()
